8816ccf3444479a78f3062332abf326a14797623
jsonjj <44034446+jsonjj@users.noreply.github.com>
2023-04-04 17:58:16 -0700

    Update cmd3.sh

--- cmd3.sh
+++ cmd3.sh
@@ -3,16 +3,17 @@ export MotorA=/sys/class/tacho-motor/motor0
 export MotorB=/sys/class/tacho-motor/motor1
 echo reset > $MotorA/command
 echo reset > $MotorB/command
-radius=5
+radius=3
 pi=3.14
-circumference=$(echo "$pi * 2 * $radius" | bc)
-revolutions=$(echo "$where / $circumference" | bc)
-degrees=$(echo "$revolutions * 360" | bc)
-pos=$(echo "$degrees - (cat ${MotorA}/position)" | bc)
-echo -e "\nRelative Position: ${pos}, Radius: ${radius}, Pi: ${pi}, Circumference: ${circumference}, Revolutions: ${revolutions}, Where: ${where} \n"
-echo $pos > $MotorA/position_sp
-echo $pos > $MotorA/position_sp
+width=12
+circumference=$(echo "scale=5; $pi * 2 * $radius" | bc)
+revolutions=$(echo "scale=5; $where / $circumference / 300 * $width" | bc)
+tmp_degrees=$(echo "$revolutions * 360" | bc)
+degrees=$(printf "%3.0f" $tmp_degrees)
+echo -e "\nDegrees: ${degrees}, Radius: ${radius}, Pi: ${pi}, Circumference: ${circumference}, Revolutions: ${revolutions}, Where: ${where}, Width: ${width} \n"
+echo $degrees > $MotorA/position_sp
+echo $degrees > $MotorB/position_sp
 echo 40 > $MotorA/speed_sp
 echo 40 > $MotorB/speed_sp
-echo run-to-rel-pos > $MotorA/command &
-echo run-to-rel-pos > $MotorB/command
+echo run-to-abs-pos > $MotorA/command &
+echo run-to-abs-pos > $MotorB/command
