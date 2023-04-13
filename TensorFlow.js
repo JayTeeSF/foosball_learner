a97ff8751e7d41f22a41a56dc5e42dbfd018f6d9
jsonjj <44034446+jsonjj@users.noreply.github.com>
2023-04-13 08:34:31 -0700

    Update TensorFlow.js

--- TensorFlow.js
+++ TensorFlow.js
@@ -1,39 +1,58 @@
-// Load the model
-const model = await tf.loadGraphModel('model.json');
+<!DOCTYPE html>
+<head>
+  <title>Foosball training from video</title>
+  <style>
+canvas {
+  border: solid 2px;
+}
+  </style>
+  <script>
+    // Load the model
+    const model = await tf.loadGraphModel('model.json');
 
-// Load the image
-const img = new Image();
-img.src = 'ball.jpg';
-await img.decode();
+    // Load the image
+    const img = new Image();
+    img.src = 'ball.jpg';
+    await img.decode();
 
-// Create a tensor from the image
-const tensor = tf.browser.fromPixels(img);
+    // Create a tensor from the image
+    const tensor = tf.browser.fromPixels(img);
 
-// Resize the tensor to match the input size of the model
-const resized = tf.image.resizeBilinear(tensor, [224, 224]);
+    // Resize the tensor to match the input size of the model
+    const resized = tf.image.resizeBilinear(tensor, [224, 224]);
 
-// Normalize the pixel values
-const offset = tf.scalar(255);
-const normalized = resized.div(offset);
+    // Normalize the pixel values
+    const offset = tf.scalar(255);
+    const normalized = resized.div(offset);
 
-// Add a batch dimension to the tensor
-const batched = normalized.expandDims(0);
+    // Add a batch dimension to the tensor
+    const batched = normalized.expandDims(0);
 
-// Make predictions with the model
-const predictions = await model.predict(batched).data();
+    // Make predictions with the model
+    const predictions = await model.predict(batched).data();
 
-// Get the coordinates of the ball
-const x1 = predictions[0] * img.width;
-const y1 = predictions[1] * img.height;
-const x2 = predictions[2] * img.width;
-const y2 = predictions[3] * img.height;
+    // Get the coordinates of the ball
+    const x1 = predictions[0] * img.width;
+    const y1 = predictions[1] * img.height;
+    const x2 = predictions[2] * img.width;
+    const y2 = predictions[3] * img.height;
+    const xCoord = (x1 + x2) / 2
+    const yCoord = (y1 + y2) / 2
+    console.log("x = " + xCoord + ", y = " + yCoord)
 
-// Draw a bounding box around the ball
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-ctx.drawImage(img, 0, 0);
-ctx.beginPath();
-ctx.rect(x1, y1, x2 - x1, y2 - y1);
-ctx.strokeStyle = 'red';
-ctx.lineWidth = 3;
-ctx.stroke();
+    // Draw a bounding box around the ball
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    ctx.beginPath();
+    ctx.rect(x1, y1, x2 - x1, y2 - y1);
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 3;
+    ctx.stroke();
+  </script>
+<body>
+    <center>
+        <canvas id="canvas"></canvas>
+    </center>
+</body>
+</html>
